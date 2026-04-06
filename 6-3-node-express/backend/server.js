3b01c16d957810addae73efcd7c9691a2cb8eacb
s202223720 <s202223720@example.com>
2026-04-06 13:05:24 +0300

    todo6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -13,12 +13,18 @@ app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
 import morgan from "morgan";
+import { getRandomQuote } from "./quotes";
 app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+     res.send("Welcome to random qoute generator!");
+   });
 
 // TODO 6.2: Create "/api/quote" route
-
+app.get("/api/quote", (req, res) => {
+     const quote = getRandomQuote();
+     res.json({ quote });
+   });
 
 // TODO 7: Start server using app.listen
